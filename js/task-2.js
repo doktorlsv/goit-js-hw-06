dac4c3717178ead0bb6cbff1ee0312d17e7838fd
doktorlsv <doktorlsv@gmail.com>
2025-07-03 01:07:45 +0300

    update1

---
+++ js/task-2.js
@@ -0,0 +1,35 @@
+console.log("task 2");
+
+class Storage {
+  #items;
+  constructor(incomingArray) {
+    this.#items = incomingArray;
+  }
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+        this.#items.splice(index, 1);
+    }
+}
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+console.log("end of task 2 ---");
